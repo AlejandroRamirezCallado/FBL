e72fd7ec75b52d43d19435106f769dfe5594fffe
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-02-26 22:04:32 +0100

    Facebook2

--- app/src/main/java/com/example/alejandro/appmarzo/FaceBActivity.java
+++ app/src/main/java/com/example/alejandro/appmarzo/FaceBActivity.java
@@ -1,13 +1,61 @@
 package com.example.alejandro.appmarzo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class FaceBActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_b);
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        // If using in a fragment
+        //loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.v("FacebookActivity", "Me he logeado en fb");
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                Log.v("FacebookActivity", "se ha logeado en fb");
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.v("FacebookActivity", "error");
+                // App code
+            }
+        });
     }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
 }
